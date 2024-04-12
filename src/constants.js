1d04bba883336ea2c4986400e13f426ee26ca724
Debmalya <debmalyamukherjee660@gmail.com>
2024-04-12 23:17:35 +0530

    main

---
+++ src/constants.js
@@ -0,0 +1,21 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
+    I've made a couple of projects in that language. I also like Tailwind and ViteJS. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/debmalya2003" target="_blank">Github</a>!`,
+  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a mixture of theoretical and practical domains but I think it gave me a good foundation.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
+  I have been recently learning more about various web-dev languages via some open-source tutorials. I would like to recommend <a href="https://youtu.be/wy_fSStEgMs?si=mRHyH4hg1aF2mDGR" target="_blank"> this channel</a>! 
+  You'll learn how I built this portfolio you're currently playing through!`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@CodeWithHarry" target="_blank">CodeWithHarry</a>, <a href="https://www.youtube.com/@DaveGrayTeachesCode" target="_blank">Dave Gray</a>,
+  <a href="https://www.youtube.com/@BroCodez" target="_blank">BroCode</a> (sometimes) and <a href="https://www.youtube.com/@takeUforward" target="_blank">Take-U-Forward</a>!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/file/d/19MzVKtxq9dy6GTveP1dWcIuwTxvIBEZE/view?usp=sharing" target="_blank">Check it out!</a>
+  Contact me at debmalyamukherjee660@gmail.com if you have any interesting project ideas!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+  library: `There are a lot of programming books on my shelves. I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+  exit: `If you want to exit Debmalya's portfolio, just close the tab.`,
+};
